8a89b6619b25bf8287febb15db7a3d50992f5a0b
Tomáš Střecha <strechatomas@outlook.com>
2022-11-28 20:44:25 +0100

    Revert "created Task and TimeReport model"
    This reverts commit 19e0a538ea91e3f2bcab6734253ddee7e9735ecf.

--- tax_manager/taxes/models.py
+++ tax_manager/taxes/models.py
@@ -1,19 +1,3 @@
 from django.db import models
 
 # Create your models here.
-
-
-class Task(models.Model):
-    name = models.models.CharField(max_length=50)
-    custom_id = models.IntegerField()
-    #money_made
-
-class TimeReport(models.Model):
-    minutes_spent = models.PositiveIntegerField()
-    task_id = models.ForeignKey(Task.pk)
-    created_at = models.DateField(auto_now_add=True)
-    modified_at = models.DateField(auto_now_add=True)
-    reported_for = models.DateField()
-    note = models.TextField()
-
-
