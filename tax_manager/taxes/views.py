a66c46d2fda0490d3caa1676149ba59303cd92da
Tomáš Střecha <strechatomas@outlook.com>
2022-11-29 20:49:02 +0100

    Added basic health check api

--- tax_manager/taxes/views.py
+++ tax_manager/taxes/views.py
@@ -1,37 +1,35 @@
+from django.http import JsonResponse
 from django.shortcuts import render
 from django.views.generic import *
+from datetime import datetime
 
-class IndexView(View):
-    
-
-    def get(self,request):
-        pass
-
-    def post(self,request):
-        pass
+def health_check(request):
+    response = {
+        'success': True,
+        'timestamp': datetime.now(tz=None)
+    }
+    return JsonResponse(response)
 
 class LoginView(View):
-    
 
-    def get(self,request):
+    def get(self, request):
         pass
 
-    def post(self,request):
+    def post(self, request):
         pass
 
 class RegisterView(CreateView):
     
-    def get(self,request):
+    def get(self, request):
         pass
 
-    def post(self,request):
+    def post(self, request):
         pass
 
 class TaskCreateView(CreateView):
     pass
 
 class TaskListView(ListView):
-    #template_name = 'tasks/task_list.html
     def get_context_data(self, request):
         pass
 
