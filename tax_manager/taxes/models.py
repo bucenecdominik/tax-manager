7fe0ceed5b45e10e5e49050a187d54d7407537ab
bucenecdominik <Db210420033@gmail.com>
2022-11-29 20:02:25 +0100

    created model report summary and added some views

--- tax_manager/taxes/models.py
+++ tax_manager/taxes/models.py
@@ -1,16 +1,47 @@
 from django.db import models
-
+from django.contrib.postgres.fields import DateRangeField
 
 class Task(models.Model):
     name = models.models.CharField(max_length=255)
     custom_id = models.CharField(max_length=50)
-    #money_made
+    #money_made = minutes_spent * user.money_per_hour
 
 class TimeReport(models.Model):
+    class State(models.TextChoices):
+        new = 'NEW'
+        reported = 'REPORTED'
+        processed = 'PROCESSED'
+        deleted = 'DELETED'
+
     minutes_spent = models.PositiveIntegerField()
-    task_id = models.ForeignKey(Task.pk)
+    task_id = models.ForeignKey(Task.custom_id)
     created_at = models.DateField(auto_now_add=True)
-    modified_at = models.DateField(auto_now_add=True)
+    modified_at = models.DateField(auto_now=True)
     reported_for = models.DateField()
-    note = models.TextField(null = True)
+    note = models.TextField(null = True, blank=True)
+    status = models.CharField(max_length=50, choices=State.choices, default=State.new)
+
+class ReportSummary(models.Model):
+    class State(models.TextChoices):
+        new = 'NEW'
+        approved = 'APPROVED'
+        declined = 'DECLINED'
+
+    name = models.CharField(max_length=255)
+    time_reports = models.ManyToManyField(TimeReport)
+    minutes_sum = models.PositiveIntegerField()
+    date_range = DateRangeField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    modified_at = models.DateTimeField(auto_now=True)
+    note = models.TextField(null=True, blank=True)
+    status = models.CharField(max_length = 50, choices=State.choices, default=State.new)
+
+
+    
+
+    
+    
+
+
+
 
--- tax_manager/taxes/views.py
+++ tax_manager/taxes/views.py
@@ -1,3 +1,59 @@
 from django.shortcuts import render
+from django.views.generic import *
 
-# Create your views here.
+class IndexView(View):
+    #template_name = 'index.html'
+
+    def get(self,request):
+        pass
+
+    def post(self,request):
+        pass
+
+class LoginView(View):
+    #template_name = 'auth/login.html'
+
+    def get(self,request):
+        pass
+
+    def post(self,request):
+        pass
+
+class RegisterView(CreateView):
+    #template_name = 'auth/register.html'
+    #form = RegisterForm
+    def get(self,request):
+        pass
+
+    def post(self,request):
+        pass
+
+class TaskCreateView(CreateView):
+    #template_name = 'tasks/task_create.html'
+    #form = TaskCreateForm
+    #success_url = 'task_list'
+    pass
+
+class TaskListView(ListView):
+    #template_name = 'tasks/task_list.html
+    def get_context_data(self, request):
+        pass
+
+    def get(self, request):
+        pass
+
+class TaskUpdateView(UpdateView):
+    #template_name = 'tasks/taks_update.html'
+    #form = TaskUpdateForm
+    #succes_url = 'task_list'
+    pass
+
+class TaskDeleteView(DeleteView):
+    #template_name = 'tasks/task_delete.html'
+    pass
+
+
+
+    
+
+
